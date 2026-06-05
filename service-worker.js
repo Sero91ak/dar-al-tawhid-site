3342620d24a3d716ba615a324ad665c3a8a5a756
Serhat Abu Malik <73606501+Sero91ak@users.noreply.github.com>
2026-06-05 08:42:28 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,10 @@
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
