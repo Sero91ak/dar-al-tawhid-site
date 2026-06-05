f4351a82b0730836c708cc95eadfeaf5298866ac
Serhat Abu Malik <73606501+Sero91ak@users.noreply.github.com>
2026-06-05 12:14:33 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,6 @@
+/* DAR AL TAWḤID – PWA + OneSignal Web Push Service Worker */
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
