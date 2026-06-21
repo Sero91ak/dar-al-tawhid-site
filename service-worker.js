18f580c41163fec4d71215c50c715ebb4e1043d9
Serhat <sero91ak@gmail.com>
2026-06-21 18:32:37 +0200

    Release compact post and dua after sections

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    Hinweis: OneSignal nutzt eigenen Service Worker unter /push/onesignal/ und wird hier nicht verändert.
 */
 
-const CACHE_VERSION = 'dar-al-tawhid-offline-light-v148';
+const CACHE_VERSION = 'dar-al-tawhid-offline-light-v149';
 const APP_SHELL = [
   '/',
   '/index.html',
