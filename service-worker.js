45bb52857aa53652d09c8b62bcee69b5b36cdf79
Serhat <sero91ak@gmail.com>
2026-06-21 13:44:46 +0200

    Shape test dock as side capsule

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    Hinweis: OneSignal nutzt eigenen Service Worker unter /push/onesignal/ und wird hier nicht verändert.
 */
 
-const CACHE_VERSION = 'dar-al-tawhid-offline-light-v138';
+const CACHE_VERSION = 'dar-al-tawhid-offline-light-v139';
 const APP_SHELL = [
   '/',
   '/index.html',
