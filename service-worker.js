e0b56513e097d1177622f0af758d71801475431a
Serhat Abu Malik <73606501+Sero91ak@users.noreply.github.com>
2026-07-14 00:01:02 +0200

    Bump service worker cache for Test App v231

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    Hinweis: OneSignal nutzt eigenen Service Worker unter /push/onesignal/ und wird hier nicht verändert.
 */
 
-const CACHE_VERSION = 'dar-al-tawhid-offline-light-v229';
+const CACHE_VERSION = 'dar-al-tawhid-offline-light-v231';
 const APP_SHELL = [
   '/',
   '/index.html',
