65a8f2f868e85318e66886fd709483e9f52535e6
Serhat Abu Malik <73606501+Sero91ak@users.noreply.github.com>
2026-07-14 13:42:26 +0200

    Premium Ilm chat redesign v234: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    Hinweis: OneSignal nutzt eigenen Service Worker unter /push/onesignal/ und wird hier nicht verändert.
 */
 
-const CACHE_VERSION = 'dar-al-tawhid-offline-light-v233';
+const CACHE_VERSION = 'dar-al-tawhid-offline-light-v234';
 const APP_SHELL = [
   '/',
   '/index.html',
