07a66819fa2e3548c9e5fa826e2ca3e95cec59ef
Serhat Abu Malik <73606501+Sero91ak@users.noreply.github.com>
2026-07-15 23:22:00 +0200

    Restore Din-Quiz in test app v241

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    Hinweis: OneSignal nutzt eigenen Service Worker unter /push/onesignal/ und wird hier nicht verändert.
 */
 
-const CACHE_VERSION = 'dar-al-tawhid-offline-light-v240';
+const CACHE_VERSION = 'dar-al-tawhid-offline-light-v241';
 const APP_SHELL = [
   '/',
   '/index.html',
