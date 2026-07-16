9201e6b922df3f400de2cef27ee16c86bab312ef
Serhat Abu Malik <73606501+Sero91ak@users.noreply.github.com>
2026-07-16 15:10:32 +0200

    Restore visitor app to last working shell v244

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    Hinweis: OneSignal nutzt eigenen Service Worker unter /push/onesignal/ und wird hier nicht verändert.
 */
 
-const CACHE_VERSION = 'dar-al-tawhid-offline-light-v243';
+const CACHE_VERSION = 'dar-al-tawhid-offline-light-v244';
 const APP_SHELL = [
   '/',
   '/index.html',
